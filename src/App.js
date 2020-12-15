298504a6f40b8419c226e45bdd52a6cfbab61d80
sobha-art <srilakshmi7995@gmail.com>
2020-12-15 12:38:25 +0530

    deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import profileimg from './img/sea.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My react app</h1>
+      <h2>Author:sobha</h2>
+      <img src={profileimg} alt="profile-img" />
+    </div>
+  );
+}
+
+export default App;
